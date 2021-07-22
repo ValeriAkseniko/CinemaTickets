1f9bee37c318b8282f3ebf8c33a11b2c111ad17b
ValerI Akseniko <valeri.akseniko@yandex.ru>
2021-07-22 22:08:16 +0300

    new migrations

---
+++ CinemaTickets/Migrations/202107221902438_addTableGenreAndCashiers.cs
@@ -0,0 +1,37 @@
+﻿namespace CinemaTickets.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class addTableGenreAndCashiers : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Cashiers",
+                c => new
+                    {
+                        Id = c.Guid(nullable: false),
+                        FullName = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Genres",
+                c => new
+                    {
+                        Id = c.Guid(nullable: false),
+                        Title = c.String(),
+                        Description = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Genres");
+            DropTable("dbo.Cashiers");
+        }
+    }
+}
