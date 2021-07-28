93889853f8bd02512df967cde8e9d8f1274ea105
ValerI Akseniko <valeri.akseniko@yandex.ru>
2021-07-28 20:55:33 +0300

    create CRUD service status

---
+++ CinemaTickets/Migrations/202107281749494_addTableStatuses.cs
@@ -0,0 +1,26 @@
+﻿namespace CinemaTickets.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class addTableStatuses : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Status",
+                c => new
+                    {
+                        Id = c.Guid(nullable: false),
+                        Name = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Status");
+        }
+    }
+}
---
+++ CinemaTickets/Services/StatusCRUDService.cs
@@ -0,0 +1,101 @@
+﻿using CinemaTickets.GlobalInterfaces;
+using CinemaTickets.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CinemaTickets.Services
+{
+    public class StatusCRUDService : IStatusCRUDService
+    {
+        public bool Create(Status status)
+        {
+            try
+            {
+                using (TicketContext db = new TicketContext())
+                {
+                    db.Statuses.Add(status);
+                    db.SaveChanges();
+                }
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+        public bool Delete(Guid id)
+        {
+            try
+            {
+                Status entity = Get(id);
+                using (TicketContext db = new TicketContext())
+                {
+                    db.Entry(entity).State = EntityState.Deleted;
+                    db.Statuses.Remove(entity);
+                    db.SaveChanges();
+                }
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+        public Status Get(Guid id)
+        {
+            try
+            {
+                using (TicketContext db = new TicketContext())
+                {
+                    Status status = db.Statuses.FirstOrDefault(x => x.Id == id);
+                    return status;
+                }
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+
+        public List<Status> List()
+        {
+            try
+            {
+                using (TicketContext db = new TicketContext())
+                {
+                    List<Status> statuses = db.Statuses.ToList();
+                    return statuses;
+                }
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+
+        public bool Update(Status status, Guid id)
+        {
+            try
+            {
+                Status entityFromDb = Get(id);
+                using (TicketContext db = new TicketContext())
+                {
+                    entityFromDb.Name = status.Name;
+                    db.Entry(entityFromDb).State = EntityState.Modified;
+                    db.SaveChanges();
+                }
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
