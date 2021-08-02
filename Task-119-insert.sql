fa8662bb13ecbe32a65917811497841fb4d4a50a
ValerI Akseniko <valeri.akseniko@yandex.ru>
2021-08-02 16:57:11 +0300

    create delete sql query and fix encoding problems

--- Task-119-insert.sql
+++ Task-119-insert.sql
@@ -1,9 +1,9 @@
-﻿INSERT Cashiers VALUES (NEWID(),'Mihail L.A.')
-INSERT Cashiers VALUES (NEWID(),'Maksim G.V.')
+﻿INSERT Cashiers VALUES (NEWID(),N'Mihail L.A.')
+INSERT Cashiers VALUES (NEWID(),N'Maksim G.V.')
 
-INSERT AgeRestrictions VALUES(NEWID(),'15','15+','что то лайтовое')
-INSERT AgeRestrictions VALUES(NEWID(),'18','18+','сцены курения и насилия')
+INSERT AgeRestrictions VALUES(NEWID(),N'15',N'15+',N'что то лайтовое')
+INSERT AgeRestrictions VALUES(NEWID(),N'18',N'18+',N'сцены курения и насилия')
 
-INSERT Genres VALUES(NEWID(),'action','action film')
-INSERT Genres VALUES(NEWID(),'drama','drama film')
+INSERT Genres VALUES(NEWID(),N'action',N'action film')
+INSERT Genres VALUES(NEWID(),N'drama',N'drama film')
 
---
+++ Task-121-delete.sql
@@ -0,0 +1,8 @@
+﻿DELETE AgeRestrictions
+WHERE Id = '53C48AA2-3DC9-4D1A-881B-7981A26165BD'
+
+DELETE Cashiers
+WHERE FullName = N'Nikolai Reptail'
+
+DELETE Genres
+WHERE Description = N'dasd'
