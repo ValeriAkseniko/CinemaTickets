2cdde4ec780a23fe5b0e53cfbdcae33e5682fc6c
ValerI Akseniko <valeri.akseniko@yandex.ru>
2021-08-02 16:42:02 +0300

    create update sql query

---
+++ Task-120-update.sql
@@ -0,0 +1,11 @@
+﻿UPDATE Cashiers
+SET FullName = 'Nikolai Reptail'
+WHERE Id = 'F6C72AEF-4E07-46E7-B8C5-04652017BCB9' 
+
+UPDATE AgeRestrictions
+SET Description = 'ну для тех кому больше 15'
+WHERE Id = 'F0BA2205-8B77-477D-B12C-5D7FAFC8E4E3'
+
+UPDATE Genres
+SET Description = 'звездалеты и лазерные мечи',Title = 'Fantasy'
+WHERE Id = '7D6A503A-2138-489B-867A-552F2E1BDCB1'
