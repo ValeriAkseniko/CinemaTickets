b4b88655fdee1a10eaa84197e9c586fee2103a7a
ValerI Akseniko <valeri.akseniko@yandex.ru>
2021-08-03 21:19:32 +0300

    added Place dtos

---
+++ CinemaTickets/DataTransferObjects/Place/PlaceViewDTO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CinemaTickets.DataTransferObjects.Place
+{
+    public class PlaceViewDTO
+    {
+        public Guid Id { get; set; }
+        public int Number { get; set; }
+        public int Capacity { get; set; }
+
+        public Guid RowId { get; set; }
+        public int RowNumber { get; set; }
+        public Guid HallId { get; set; }
+        public string HallTitle { get; set; }
+    }
+}
