5f93ed9bb0a825daaf5264ef233c97ae56150062
Artem Lipinskiy <artem_lipinskiy@mail.ru>
2021-08-07 15:36:38 +0300

    Updated IPlaceCRUDService, PlaceCRUDService Created PlaceCreateDTO

---
+++ CinemaTickets/DataTransferObjects/Place/PlaceCreateDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CinemaTickets.DataTransferObjects.Place
+{
+    public class PlaceCreateDTO
+    {
+        public Guid Id { get; set; }
+        public int Number { get; set; }
+        public int Capacity { get; set; }
+
+        public Guid RowId { get; set; }
+    }
+}
--- CinemaTickets/GlobalInterfaces/IPlaceCRUDService.cs
+++ CinemaTickets/GlobalInterfaces/IPlaceCRUDService.cs
@@ -4,12 +4,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CinemaTickets.DataTransferObjects.Place;
 
 namespace CinemaTickets.GlobalInterfaces
 {
     public interface IPlaceCRUDService
     {
-        bool Create(Place place); 
+        bool Create(PlaceCreateDTO place); 
         
         Place Get(Guid id); 
         
