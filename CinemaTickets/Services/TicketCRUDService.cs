71ffce7cac97bc0363ef60b04e45565f268e7c79
ValerI Akseniko <valeri.akseniko@yandex.ru>
2021-08-07 14:52:51 +0300

    create ticket CRUD service

---
+++ CinemaTickets/Services/TicketCRUDService.cs
@@ -0,0 +1,116 @@
+﻿using CinemaTickets.GlobalInterfaces;
+using CinemaTickets.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data.Entity;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CinemaTickets.Services
+{
+    public class TicketCRUDService : ITicketCRUDService
+    {
+        public bool Create(Ticket ticket)
+        {
+            try
+            {
+                using (TicketContext db = new TicketContext())
+                {
+                    db.Tickets.Add(ticket);
+                    db.SaveChanges();
+                }
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+        public bool Delete(Guid id)
+        {
+            try
+            {
+                Ticket entity = Get(id);
+                using (TicketContext db = new TicketContext())
+                {
+                    db.Entry(entity).State = EntityState.Deleted;
+                    db.Tickets.Remove(entity);
+                    db.SaveChanges();
+                }
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+        public Ticket Get(Guid id)
+        {
+            try
+            {
+                using (TicketContext db = new TicketContext())
+                {
+                    Ticket entity = db.Tickets
+                        .Include(x => x.Place)
+                        .Include(x =>x.Place.Row)
+                        .Include(x => x.Status)
+                        .Include(x => x.Cashier)
+                        .Include(x => x.Film)
+                        .Include(x => x.Film.AgeRestriction)
+                        .Include(x => x.Film.Genre)
+                        .FirstOrDefault(x => x.Id == id);
+                    return entity;
+                }
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+
+        public List<Ticket> List()
+        {
+            try
+            {
+                using (TicketContext db = new TicketContext())
+                {
+                    List<Ticket> tickets = db.Tickets.ToList();
+                    return tickets;
+                }
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+
+        public bool Update(Ticket ticket, Guid id)
+        {
+            try
+            {
+                Ticket entityFromDb = Get(id);
+                using (TicketContext db = new TicketContext())
+                {
+                    entityFromDb.Cashier = ticket.Cashier;
+                    entityFromDb.DateOfSale = ticket.DateOfSale;
+                    entityFromDb.Film = ticket.Film;
+                    entityFromDb.Place = ticket.Place;
+                    entityFromDb.Price = ticket.Price;
+                    entityFromDb.Start = ticket.Start;
+                    entityFromDb.Status = ticket.Status;
+                    entityFromDb.TypeOfCalculation = ticket.TypeOfCalculation;
+                    db.Entry(entityFromDb).State = EntityState.Modified;
+                    db.SaveChanges();
+                }
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+    }
+}
