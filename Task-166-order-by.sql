38e8567961b0d948cb2533796d15f436b54dd86b
ValerI Akseniko <valeri.akseniko@yandex.ru>
2021-08-31 14:10:42 +0300

    create sql query order by

---
+++ Task-166-order-by.sql
@@ -0,0 +1,18 @@
+﻿use TicketDB;
+select Id,Capacity from Places
+order by Capacity
+
+
+use TicketDB;
+select Id,Capacity from Places
+order by Capacity desc
+
+
+use TicketDB;
+select Title,MinAge from AgeRestrictions
+order by MinAge
+
+
+use TicketDB;
+select Title,MinAge from AgeRestrictions
+order by MinAge desc
