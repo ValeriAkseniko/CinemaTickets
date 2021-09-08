049e6692710d509d666a0718b7506f5ef67f4e10
ValerI Akseniko <valeri.akseniko@yandex.ru>
2021-09-09 00:19:13 +0300

    ti mne ne platish` chtob yvolit`

--- task-198-sql-query-capacity.sql
+++ task-198-sql-query-capacity.sql
@@ -1,11 +1,9 @@
 ﻿use TicketDB;
 select sum(Capacity)
-from Places,Rows,Halls
-where Places.RowId = Rows.Id and Rows.HallId = Halls.Id
---Hall.id = id звлв
+from Places,Rows
+where Places.RowId = Rows.Id and Rows.HallId = '1BD4C29B-CD0B-4909-ADAE-CA1B4B461E8C';
 
 use TicketDB;
 select sum(Capacity)
-from Places,Rows
-where Places.RowId = Rows.Id
---Row.id = id ряда
+from Places
+where Places.RowId = 'DAD80150-5083-4BCA-AF78-307CE73C8A40';
