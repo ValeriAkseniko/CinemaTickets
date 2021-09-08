ba481771c71c5ce1577c7148fed105814e7c5f01
ValerI Akseniko <valeri.akseniko@yandex.ru>
2021-09-09 00:04:55 +0300

    create sql query capacity

---
+++ task-198-sql-query-capacity.sql
@@ -0,0 +1,9 @@
+﻿use TicketDB;
+select sum(Capacity)
+from Places,Rows,Halls
+where Places.RowId = Rows.Id and Rows.HallId = Halls.Id
+
+use TicketDB;
+select sum(Capacity)
+from Places,Rows
+where Places.RowId = Rows.Id
