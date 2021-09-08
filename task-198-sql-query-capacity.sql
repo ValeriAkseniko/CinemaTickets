61a1b08469ad2e33934b7c1e96771075304ca97f
ValerI Akseniko <valeri.akseniko@yandex.ru>
2021-09-09 00:10:27 +0300

    fix

--- task-198-sql-query-capacity.sql
+++ task-198-sql-query-capacity.sql
@@ -2,8 +2,10 @@
 select sum(Capacity)
 from Places,Rows,Halls
 where Places.RowId = Rows.Id and Rows.HallId = Halls.Id
+--Hall.id = id звлв
 
 use TicketDB;
 select sum(Capacity)
 from Places,Rows
 where Places.RowId = Rows.Id
+--Row.id = id ряда
