0ce964cd81c6dfae871172c3a9b878f64c6966f2
Artem Lipinskiy <artem_lipinskiy@mail.ru>
2021-09-09 12:13:38 +0300

    rewrite Capacity for Hall

--- task-198-sql-query-capacity.sql
+++ task-198-sql-query-capacity.sql
@@ -1,7 +1,27 @@
 ﻿use TicketDB;
-select sum(Capacity)
-from Places,Rows
-where Places.RowId = Rows.Id and Rows.HallId = '1BD4C29B-CD0B-4909-ADAE-CA1B4B461E8C';
+SELECT SUM(HallInfo.Capacity) AS Capacity
+FROM ( 
+	SELECT
+		Halls.Id,
+		Halls.Title,
+		HallRows.RowNumber AS RowNumber,
+		HallRows.PlaceNumber AS PlaceNumber, 
+		HallRows.Capacity As Capacity
+	FROM Halls
+	LEFT JOIN (
+		SELECT 
+			Rows.HallId, 
+			Rows.Id, 
+			Rows.Number AS RowNumber, 
+			Places.Number AS PlaceNumber, 
+			Places.Capacity 
+		FROM Rows
+		LEFT JOIN Places 
+		ON Places.RowId = Rows.Id
+	) AS HallRows 
+	ON Halls.Id = HallRows.HallId 
+) AS HallInfo
+WHERE HallInfo.Id = '1BD4C29B-CD0B-4909-ADAE-CA1B4B461E8C'
 
 use TicketDB;
 select sum(Capacity)
