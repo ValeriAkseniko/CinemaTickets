ee2e2fba6f5a7c4c16277cfecc2f90b0996e809f
Artem Lipinskiy <artem_lipinskiy@mail.ru>
2021-09-09 12:24:13 +0300

    remove unused info

--- task-198-sql-query-capacity.sql
+++ task-198-sql-query-capacity.sql
@@ -2,18 +2,12 @@
 SELECT SUM(HallInfo.Capacity) AS Capacity
 FROM ( 
 	SELECT
-		Halls.Id,
-		Halls.Title,
-		HallRows.RowNumber AS RowNumber,
-		HallRows.PlaceNumber AS PlaceNumber, 
+		Halls.Id AS HallId,
 		HallRows.Capacity As Capacity
 	FROM Halls
 	LEFT JOIN (
 		SELECT 
 			Rows.HallId, 
-			Rows.Id, 
-			Rows.Number AS RowNumber, 
-			Places.Number AS PlaceNumber, 
 			Places.Capacity 
 		FROM Rows
 		LEFT JOIN Places 
@@ -21,7 +15,7 @@ FROM (
 	) AS HallRows 
 	ON Halls.Id = HallRows.HallId 
 ) AS HallInfo
-WHERE HallInfo.Id = '1BD4C29B-CD0B-4909-ADAE-CA1B4B461E8C'
+WHERE HallInfo.HallId = '1BD4C29B-CD0B-4909-ADAE-CA1B4B461E8C'
 
 use TicketDB;
 select sum(Capacity)
