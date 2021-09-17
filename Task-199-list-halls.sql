81116cb03eed381578f13a4c60e7daf57ca5c733
ValerI Akseniko <valeri.akseniko@yandex.ru>
2021-09-17 14:41:51 +0300

    create sql query

---
+++ Task-199-list-halls.sql
@@ -0,0 +1,6 @@
+﻿use TicketDB;
+select Halls.Id,Halls.Title,SUM(Places.Capacity) AS Capacity
+from Halls
+join Rows on Halls.Id = Rows.HallId
+join Places on Places.RowId = Rows.Id
+Group by Halls.Title,Halls.id 
