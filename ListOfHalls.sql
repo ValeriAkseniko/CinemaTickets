6186bdda50f5f28c5de09a3f54ec8cb0ac75cb5a
ValerI Akseniko <valeri.akseniko@yandex.ru>
2021-10-04 15:06:41 +0300

    list of halls change

--- ListOfHalls.sql
+++ ListOfHalls.sql
@@ -1,12 +1,15 @@
-﻿SELECT        dbo.Halls.Id, dbo.Halls.Title, SUM(dbo.Places.Capacity) AS CapacityHall,
-                             (SELECT        COUNT(*) AS Expr1
-                               FROM            dbo.Rows
-                               WHERE        (HallId = dbo.Halls.Id)) AS CountRow,
-                             (SELECT        COUNT(*) AS Expr1
-                               FROM            dbo.Places INNER JOIN
-                                                         dbo.Rows ON dbo.Places.RowId = dbo.Rows.Id
-                               WHERE        (dbo.Rows.HallId = dbo.Halls.Id)) AS CountPlace
-FROM            dbo.Halls INNER JOIN
-                         dbo.Rows ON dbo.Rows.HallId = dbo.Halls.Id INNER JOIN
-                         dbo.Places ON dbo.Places.RowId = dbo.Rows.Id
-GROUP BY dbo.Halls.Id, dbo.Halls.Title
+﻿use TicketDB
+go
+create view ListOfHalls as
+select  Halls.Id,
+		Halls.Title, 
+		sum(dbo.Places.Capacity) as CapacityHall,
+           (select count(*) AS Expr1
+            from Rows
+            where (HallId = Halls.Id)) AS CountRow,
+            (select count(*) AS Expr1
+            from Places join Rows ON Places.RowId = Rows.Id
+            where (Rows.HallId = Halls.Id)) AS CountPlace
+from Halls join Rows on Rows.HallId = Halls.Id
+           join Places on Places.RowId = Rows.Id
+group by Halls.Id, Halls.Title
